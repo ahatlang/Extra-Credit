b356e8b113ce0f77452616662c365dcc99a6ff80
Andrew Hatlang <158089508+ahatlang@users.noreply.github.com>
2024-03-03 11:53:30 -0600

    Add files via upload
    Code from A.I.

---
+++ ExtraCredit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  1 10:56:30 2024 @author: andrewhatlang
+"""
+# To output the TeX equation as a string in Python, you can simply print it out.
+
+# Define the TeX equation as a Python string
+tex_equation = r'$y = \sin(x)$'
+
+# Output the TeX equation string
+print(tex_equation)
